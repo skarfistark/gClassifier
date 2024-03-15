12b0e31172ed9a65acbfbb5aef1aa109a8abb780
skarfistark <87260112+skarfistark@users.noreply.github.com>
2024-03-16 00:58:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -86,4 +86,4 @@ def prediction():
     return jsonify({'prediction': ans})
 
 if __name__ == '__main__':
-    app.run(debug=True)  # Set debug=True for development purposes
+    app.run(host="0.0.0.0" , port=5000 ,debug=True)  # Set debug=True for development purposes
